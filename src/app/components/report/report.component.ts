7618e452d82720f0579b1c8e53751cea61a71737
nadjajovanovic <j.nadja97@gmail.com>
2021-10-07 16:29:53 +0200

    Report component modified

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { Client } from 'src/app/models/client';
+import { Project } from 'src/app/models/project';
+import { Report } from 'src/app/models/report';
+import { TeamMember } from 'src/app/models/team-member';
+import { ReportService } from 'src/app/services/report.service';
 
 @Component({
   selector: 'app-report',
@@ -7,9 +13,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
+  reports: Report[] = [];
+  categories: Category[] = [];
+  teamMembers: TeamMember[] = [];
+  clients: Client[] = [];
+  pojects: Project[] =[];
+
+  constructor(private reportService : ReportService,
+    @Inject(Report) public data: Report) { }
 
   ngOnInit(): void {
+    this.reportService.getAllReports().subscribe(data => {
+      this.reports = data;
+    });
+  }
+
+  public addReport() {
+    this.reportService.addReport(this.data);
+  }
+
+  public updateReport() : void {
+    this.reportService.editReport(this.data);
+  }
+
+  public deleteReport() : void {
+    this.reportService.deleteReport(this.data.reportid);
   }
 
 }
